ccf6dce7043c53264d092e2478d70e9f7b5ba4fb
Patrycja Wozniewicz <patrycja.wozniewicz@gmail.com>
2024-03-14 14:55:40 +0100

    first commit

---
+++ src/pages/BookToRead.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useBookContext } from '../BooksContext'
+
+
+
+
+
+const BookToRead = () => {
+  const { selectedBook } = useBookContext()
+
+  if (!selectedBook || selectedBook.length === 0) {
+    <div className="mt-8">
+      <h1>No books selected</h1>
+    </div>
+  }
+
+
+
+  return (
+    <div className="mt-8">
+      <h1>Selected Books</h1>
+      {selectedBook.map((book) => (
+        <div key={book.id} className="border border-gray-300 rounded-lg p-4">
+          <h2 className="text-xl font-semibold mb-2">{book.volumeInfo.title}</h2>
+          {book.volumeInfo.authors && (
+            <p className="text-gray-600 mb-2">{book.volumeInfo.authors.join(', ')}</p>
+          )}
+          {book.volumeInfo.imageLinks && (
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} className="" />
+          )}
+          <p className="text-gray-700 mt-2">{book.volumeInfo.description}</p>
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default BookToRead
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import BookDetails from "../components/BookDetails"
+import BookSearch from "../components/BookSearch"
+
+
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className='flex items-center justify-center h-screen bg-cover bg-center opacity-70 '
+                style={{ backgroundImage: `url(https://images.pexels.com/photos/18620051/pexels-photo-18620051/free-photo-of-books-background.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1` }}
+            >
+                <BookSearch />
+            </div>
+            <BookDetails/>
+        </div>
+    )
+}
+
+export default Home
